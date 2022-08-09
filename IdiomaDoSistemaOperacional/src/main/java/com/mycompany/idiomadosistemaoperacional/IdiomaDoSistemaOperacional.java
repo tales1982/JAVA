a36e8606b6e7a7ea506aa642d44aa67360ead612
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-09 20:07:25 +0200

    IdiomaDoSistemaOperacional

--- IdiomaDoSistemaOperacional/src/main/java/com/mycompany/idiomadosistemaoperacional/IdiomaDoSistemaOperacional.java
+++ IdiomaDoSistemaOperacional/src/main/java/com/mycompany/idiomadosistemaoperacional/IdiomaDoSistemaOperacional.java
@@ -15,7 +15,7 @@ public class IdiomaDoSistemaOperacional {
 
     public static void main(String[] args) {
         Locale loc = Locale.getDefault();
-        System.out.println("O idioma do sistema é");
+        System.out.println("O idioma do sistema é -> ");
         System.out.println(loc.getDisplayLanguage(loc));
     }
 }
