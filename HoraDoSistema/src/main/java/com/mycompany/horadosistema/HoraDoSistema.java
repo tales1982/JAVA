ce23d1bf9b67dc249f7ef3cff330c5de32fb6d64
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-09 20:05:20 +0200

    HoraDoSistema

--- HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
+++ HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -12,7 +12,7 @@ public class HoraDoSistema {
 
     public static void main(String[] args) {
         Date data = new Date();
-        System.out.println("A hora do sistema é");
+        System.out.println("A hora do sistema é ->");
         System.out.println(data.toString());
     }
 }
