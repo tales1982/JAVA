46811d6269c0d4be09673ff5c603e56ddd0ec8c3
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-09 21:52:56 +0200

    AreaCircunferencia

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,7 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
+		
 		System.out.println("Primeiro programa parte 01");
 		System.out.println("Primeiro Programa parte 02");
 		System.out.println("Primeiro Programa parte 03");
