0f4395617b077ef39a38b7ba3dc1b9e9f75398c6
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-09 20:10:43 +0200

    Dimensao

--- Dimesao_Da_Tela/DimensaoDaTela/src/pacote1/Dimensao.java
+++ Dimesao_Da_Tela/DimensaoDaTela/src/pacote1/Dimensao.java
@@ -9,7 +9,7 @@ public class Dimensao {
     public static void main(String[] args) {
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension d = tk.getScreenSize();
-        System.out.println("A resolução do seu sistema é:"+ d.width + "X" + d.height);
+        System.out.println("A resolução do seu sistema é. :"+ d.width + "X" + d.height);
         System.out.println("  ");
     }
 }
