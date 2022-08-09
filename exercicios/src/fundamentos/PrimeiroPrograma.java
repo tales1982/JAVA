6cde32cf9902205ceb8573fb2a51b977df513504
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-09 21:41:25 +0200

    Primeiro Programa

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -4,6 +4,8 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Primeiro programa parte 01");
 		System.out.println("Primeiro Programa parte 02");
+		System.out.println("Primeiro Programa parte 03");
+		System.out.println("Primeiro Programa parte 04");
 		
 	}
 }
