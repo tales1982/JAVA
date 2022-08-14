a10fdb6e0694a7ef8eb177351be569f7aab3d5c3
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-14 13:31:28 +0200

    Console

--- exercicios/src/fundamentos/Console.java
+++ exercicios/src/fundamentos/Console.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Console {
 	public static void main(String[] args) {
-
+/*
 		System.out.print("Bom ");
 		System.out.print("dia!\n");
 
@@ -33,5 +33,17 @@ public class Console {
 		System.out.printf("%s %s tem %d anos. \n", nome, sobrenome, idade );
 		
 		entrada.close();
+		*/
+		Scanner teste = new Scanner (System.in);
+		System.out.println("Ola Prazer em tem conhecer. Como voce se Chama? ");
+		String nome = teste.nextLine().toUpperCase();
+		System.out.printf("Seja Bem vindo! %s %n", nome);
+		
+		System.out.printf("%s Qual e a sua idade ", nome);
+		int idade = teste.nextInt();
+		System.out.printf("%s Parabens, %d anos e uma bella idade!!", nome, idade);
+		teste.close();
+		
+		
 	}
 }
