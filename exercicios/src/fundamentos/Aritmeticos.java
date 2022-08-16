cd1db13a3815b7c3600a62bd4012743eea39c54f
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-16 19:29:46 +0200

    DesafioAritmeticos

--- exercicios/src/fundamentos/Aritmeticos.java
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -20,5 +20,7 @@ public class Aritmeticos {
 		System.out.println(a - b);
 		System.out.println(a * b);
 		System.out.println(a / b);
+		System.out.println(a / (double)b);// estou convertendo meu numero inteiro pra double para ter a resposta exata
+		System.out.println(a % b );//o resultado e o resto da divisao. 8/3= no casso 6-> 2 pra cada sobra(2)
 	}
 }
