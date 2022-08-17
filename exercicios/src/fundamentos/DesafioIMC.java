50985cd764284ba41bb91c59a0db327d3dca65ca
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-17 23:44:59 +0200

    Desafio IMC

--- exercicios/src/fundamentos/DesafioIMC.java
+++ exercicios/src/fundamentos/DesafioIMC.java
@@ -30,7 +30,7 @@ public class DesafioIMC {
             System.out.println(" Sua Classificação é de Magreza");
         }else if(imc >= 18.5 && imc <= 24.9) {
             System.out.println(" Sua Classificação Está Dentro do Normal");
-        }if(imc >=25 && imc <=29.9) {
+        }if(imc >=25 && imc <=29.9){
             System.out.println(" Sua Classificação é de Sobrepeso");
         }else if (imc >=30 && imc <=34.9){
             System.out.println(" Sua Classificação é de Obesidade grau I");
