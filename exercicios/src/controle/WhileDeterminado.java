4094cb6dbdca8602f3271a1e094c4e4ecca533db
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-18 23:13:33 +0200

    While Determinado

---
+++ exercicios/src/controle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class WhileDeterminado {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<=9){
+            i ++; //pode ser tamben assim --> i = i + 1;
+            System.out.println( i );
+        }
+    }
+}
