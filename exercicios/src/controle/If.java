1020591d5e01c3ee26c33476dace3be956cfaa17
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-18 14:53:35 +0200

    If

--- exercicios/src/controle/If.java
+++ exercicios/src/controle/If.java
@@ -1,7 +1,17 @@
 package controle;
-    
+
+import java.util.Scanner;
+
 public class If {
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+            System.out.println("Informe a media: ");
+            double media = entrada.nextDouble();
+
+            if (media >= 7.0){
+                System.out.println("Aprovado \nParabens");
+            }
+
+            entrada.close();
     }
 }
