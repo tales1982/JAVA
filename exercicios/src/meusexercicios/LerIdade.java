52d1053fba364d5e2f43988f74e8db0f9c6d41ca
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-19 18:16:31 +0200

    Ler Idade

---
+++ exercicios/src/meusexercicios/LerIdade.java
@@ -0,0 +1,19 @@
+package meusexercicios;
+
+import javax.swing.JOptionPane;
+
+public class LerIdade {
+    public static void main(String[] args) {
+        // System.out.println("Ola digite sua idade");
+
+        String valor = JOptionPane.showInputDialog("Ola digite sua idade");
+        int idade = Integer.parseInt(valor);
+
+        int ano = 365;
+        int meis = 12;
+        int dia = 30;
+
+        int res = idade 
+
+    }
+}
