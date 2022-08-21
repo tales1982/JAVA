847dc22b91b02b9b8fc07722d41ac019e8211a86
Tales Lima <tales_lima_1982@hotmail.com>
2022-08-21 16:43:36 +0200

    While Indeterminado

--- exercicios/src/controle/WhileIndeterminado.java
+++ exercicios/src/controle/WhileIndeterminado.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class WhileIndeterminado {
     public static void main(String[] args) {
-        Scannernner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         String res = "";
 
